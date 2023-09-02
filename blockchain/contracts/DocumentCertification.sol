7a8fa8bf0cde5aeaf9155ebbc4e80899fa5fbf4c
gilbert <mbula.gilberto@gmail.com>
2023-09-02 22:21:58 +0100

    blockchain code modified

--- blockchain/contracts/DocumentCertification.sol
+++ blockchain/contracts/DocumentCertification.sol
@@ -5,11 +5,9 @@ contract DocumentCertification {
     address public owner;
       
     struct Document {
-        string contentHash;  
-        uint256 timestamp;    
-        string owner; 
-        string importantInfos;
-        string fileUrl;
+        string contentHash; 
+        string fileHash;  
+        uint256 timestamp;  
     }
     
     mapping(string => Document) public certifiedDocuments;
@@ -21,36 +19,15 @@ contract DocumentCertification {
 
     constructor() public {
         owner = msg.sender;
-    }
-
-    function certifyDocument(string memory _contentHash, string memory _owner_name, string memory _smallDesc, string memory _fileUrl) public {
-        require(bytes(_contentHash).length > 0, "Content hash cannot be empty");
-        require(certifiedDocuments[_contentHash].timestamp == 0, "Document is already certified");
-        
-        Document memory newDocument = Document({
-            contentHash: _contentHash,
-            timestamp: block.timestamp,
-            owner: _owner_name,
-            importantInfos: _smallDesc,
-            fileUrl: _fileUrl
-        });
-        
-        certifiedDocuments[_contentHash] = newDocument;
-    }
-
-    
-    function getCertificationInfo(string memory _contentHash) public view returns (
-        string owner,
-        string importantInfos,
-        string fileUrl,
-
-    ) {
-        Document memory doc = certifiedDocuments[_contentHash];
-        if(doc.timestamp != 0){
-          return ("", "", "")
-        }
-        
-        return (doc.owner, doc.importantInfos, doc.fileUrl);
+    } 
+ 
+    function documentExists(string memory _contentHash) public view returns (bool) {
+        return bytes(certifiedDocuments[_contentHash].contentHash).length != 0;
     }
  
+    function saveDocument(string memory _contentHash, string memory _fileHash) public onlyOwner {
+        require(!documentExists(_contentHash), "Content hash already exists");
+        uint256 timestamp = block.timestamp;
+        certifiedDocuments[_contentHash] = Document(_contentHash, _fileHash, timestamp);
+    }
 }
--- utils/eth.py
+++ utils/eth.py
@@ -1,9 +1,20 @@
-def checkIfExist(hashText, contract):
+def checkIfExist(hashText, contract, user_address):
     #get data tasks from blockchain
-    tasks  = contract.functions.getTasks().call({'from': user_address}) 
+    return contract.functions.documentExists(hashText).call({'from': user_address})  
 
-    return False
-
+def getRecordFromBlockChain(hashText):
+    result  =  contract.functions.getDocumentInfo(hashText).call({'from': user_address})  
+    datas =  {
+            'contentHash': result[0],
+            'fileHash': result[1],
+            'timestamp': result[2]
+        }
     
-def saveRecordInBlockChain(ipfsIDHash, hashText):
+def saveRecordInBlockChain(ipfsIDHash, hashText, contract, user_address):
+    result  =  contract.functions.documentExists(hashText).call({'from': user_address})  
+    datas =  {
+            'contentHash': result[0],
+            'fileHash': result[1],
+            'timestamp': result[2]
+        }
     return True
